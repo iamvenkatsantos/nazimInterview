ad1fe8012916e94e9420ea28a2f715536f44d17e
Venkat Santos <nagularaj6119@gmail.com>
2021-02-16 20:46:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'nazim';
+  messages: Array<any> = [
+    {
+      isGroup: true,
+      count: 14,
+      name: "Special class students",
+      time: "18:31",
+      lastMessage: "Pls take a look at this message",
+      profile: "fas fa-user",
+      isYou: false,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Don Norman",
+      time: "16:04",
+      lastMessage: "Hello guys, we discussed about..",
+      profile: "fas fa-user",
+      isYou: false,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Lee Williamson",
+      time: "06:12",
+      lastMessage: "Yes, that's goona work, hopefully.",
+      profile: "fas fa-user",
+      isYou: false,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Ronald Mccoy",
+      time: "Yesterday",
+      lastMessage: "Thanks Dude",
+      profile: "fas fa-user",
+      isYou: true,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Albert Bell",
+      time: "Yesterday",
+      lastMessage: "I'm happy this chapter has gree..",
+      profile: "fas fa-user",
+      isYou: false,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Ronald Mccoy",
+      time: "Yesterday",
+      lastMessage: "Thanks Dude",
+      profile: "fas fa-user",
+      isYou: true,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Albert Bell",
+      time: "Yesterday",
+      lastMessage: "I'm happy this chapter has gree..",
+      profile: "fas fa-user",
+      isYou: false,
+    },
+    {
+      isGroup: false,
+      count: 0,
+      name: "Ronald Mccoy",
+      time: "Yesterday",
+      lastMessage: "Thanks Dude",
+      profile: "fas fa-user",
+      isYou: true,
+    },
+  ];
 }
